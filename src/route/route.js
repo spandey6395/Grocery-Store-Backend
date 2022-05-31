43cda84f0e5220d49bd4845487ae419b71cf5b42
Deepak <deepakgunpal0@gmail.com>
2022-05-31 11:59:55 +0530

    shoppingCart Completed

--- src/route/route.js
+++ src/route/route.js
@@ -4,7 +4,6 @@ const { createUser, getUser, login, updateUser, } = require("../controllers/user
 const { productById, deleteProduct, createProduct, filterProduct, updateProduct } = require("../controllers/productController");
 const { addToCart, updateCart, getCart, deleteCart } = require("../controllers/cartController")
 const { authentication, authorization } = require("../middleware/auth");
-const { route } = require("express/lib/application");
 const { createOrder, updateOrder } = require("../controllers/orderController");
 
 //feature 1 user api's
@@ -27,7 +26,7 @@ router.get("/users/:userId/cart", authentication, authorization, getCart)
 router.delete("/users/:userId/cart", authentication, authorization, deleteCart)
 
 //feature 4 cart api's
-router.post("/users/:userId/orders", createOrder)
-router.put("/users/:userId/orders", updateOrder)
+router.post("/users/:userId/orders", authentication, authorization, createOrder)
+router.put("/users/:userId/orders", authentication, authorization, updateOrder)
 
 module.exports = router;
